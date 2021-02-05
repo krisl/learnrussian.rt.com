dd5dc0b426199b1f7565bdb626aeb9bb63a75aae
Aaron Lipinski <kris.lipinski@gmail.com>
2021-02-05 22:50:53 +1300

    head processing

--- thescript.js
+++ thescript.js
@@ -4,26 +4,42 @@ const glob = require('glob');
 //const htmlparser2 = require('htmlparser2');
 
 //const data = fs.readFileSync("/home/aaron/development/aaron/learnrussian.rt.com/index.html", "utf8");
+function wrapInFrontMatter(newItems) {
+	return ["---", ...newItems, "---"].join('\n') + '\n'
+}
 
 const work = "/home/aaron/development/aaron/learnrussian.rt.com/"
 const local = "./"
 const root = local
 const allhtml = root + "**/*.html";
 const indexhtml = root + "index.html";
-
-glob(allhtml, null, (er, files) => {
+const markFm = "---\n";
+glob(indexhtml, null, (er, files) => {
     //console.log({er});
     //console.log({files});
     files.forEach(file => {
         console.log({file})
         const data = fs.readFileSync(file, "utf8");
-        const data2 = data.substring(data.indexOf("\n") + 1)
-        const data3 = data2.substring(data2.indexOf("\n") + 1)
-        //console.log({data3});
-        const $ = cheerio.load(data3) //, {frontMatter: true}, false); //, { decodeEntities: true});// , null, false);
+        const openFmPos = data.indexOf(markFm)
+        const closFmPos = data.indexOf(markFm, openFmPos + markFm.length)
+	const frontMatt = data.substring(openFmPos, closFmPos + markFm.length);
+        const htmlText = data.substring(openFmPos + frontMatt.length +1)
+        console.log({frontMatt});
+	    //console.log({htmlText});
+        const $ = cheerio.load(htmlText) //, {frontMatter: true}, false); //, { decodeEntities: true});// , null, false);
 	    //$('#footer').remove();
-	$('#footer').replaceWith('{% include footer.html %}');
-        fs.writeFileSync(file, "---\n---\n" + $.html());
+	const desc = $('meta[name="description"]').attr('content');
+	const keys = $('meta[name="keywords"]').attr('content');
+	const titl = $('title').text().trim();
+	    console.log({desc, keys})
+	const newFm = wrapInFrontMatter([
+		"description: " + desc,
+		"keywords: " + keys,
+		"title: " + titl
+	]);
+	    console.log({newFm})
+	$('#head').replaceWith('{% include head.html %}');
+        fs.writeFileSync(file, newFm + $.html());
     });
 })
 
